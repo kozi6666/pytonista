2e9282271562ff637836db6ba56159b8883521f9
kozi6666 <kubakoko63@gmail.com>
2023-08-08 05:51:23 +0200

    fizzbuzz

--- 7.pusty.py
+++ 7.pusty.py
@@ -1,50 +1,13 @@
-import random
+lsit = []
 
-list = [1, 2, 3, 4, 5, 6]
-x = 1
-nums = []
-suming = sum(nums)
+for x in range(1, 30):
+    if x % 2 == 0 and x % 3 == 0:
+        print("fizzbuzz")
+    elif x % 2 == 0:
+        print("fizz")
+    elif x % 3 == 0:
+        print("buzz")
+    else:
+        lsit.append(x)
 
-def game():
-    print("")
-    print("Witaj w grze BLACKJACK")
-    print("")
-    print("Wciśnij <enter> aby zacząć gre")
-
-
-def throw(suming,):
-    req = input("...")
-    x = 1
-    while True:
-        if req == "":
-            print(f"{x}"" Rzut kostką")
-            print("")
-            num = (random.choice(list))
-            print(num)
-            nums.append(num)
-            x += 1
-            suming = sum(nums)
-            print("")
-            print("Twoje punkty: ", suming)
-            continue
-
-        elif req == "q":
-            print("Twój ostateczny wynik to:", suming)
-            break
-        elif suming == 21:
-            print("Gratulacje! Osiągąłeś maxymalną liczbę punktów")
-            break
-        elif suming < 21:
-            print("Przegrałeś! Niestety przekroczyłeś 21 punktów")
-            break
-        else:
-            continue
-
-def suming():
-    while suming == 21:
-    print("Gratulacje! Osiągąłeś maxymalną liczbę punktów")
-    exit()
-
-    while suming < 21:
-    print("Przegrałeś! Niestety przekroczyłeś 21 punktów")
-    exit()
+print("Liczby pierwsze: ", lsit)
