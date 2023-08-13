ff2d91db127be52a11ffb14ab8f096d0ca30cf93
kozi6666 <kubakoko63@gmail.com>
2023-08-13 21:08:33 +0200

    naprawione

--- 13.racketa2.py
+++ 13.racketa2.py
@@ -1,2 +1,13 @@
+from raketa import Rocket
 from random import randint
-from 13.raketa import Rocket,
+
+rockets = [Rocket(randint(1, 6)) for _ in range(5)]
+
+for _ in range(10):
+    rocketsIndexToMove = randint(0, len(rockets) - 1)
+    rockets[rocketsIndexToMove].moveup()
+
+for rocket in rockets:
+    print(rocket)
+
+rocket = Rocket()
--- raketa.py
+++ raketa.py
@@ -13,13 +13,13 @@ class Rocket:
         return "Rakieta na wysokości: " + str(self.altitude)
 
 
-rockets = [Rocket(randint(1, 6)) for _ in range(5)]
-
-for _ in range(10):
-    rocketsIndexToMove = randint(0, len(rockets) - 1)
-    rockets[rocketsIndexToMove].moveup()
-
-for rocket in rockets:
-    print(rocket)
-
-rocket = Rocket()
+# rockets = [Rocket(randint(1, 6)) for _ in range(5)]
+#
+# for _ in range(10):
+#     rocketsIndexToMove = randint(0, len(rockets) - 1)
+#     rockets[rocketsIndexToMove].moveup()
+#
+# for rocket in rockets:
+#     print(rocket)
+#
+# rocket = Rocket()
